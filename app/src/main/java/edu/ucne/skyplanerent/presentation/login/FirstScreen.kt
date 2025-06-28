0e866e7f462e5079bb56ab9b9fab15d2caa64d24
fahh7 <fahh7@FREIMYHP>
2025-06-28 19:39:15 -0400

    Añadiendo imagen y cambio de tamaño de letras

--- app/src/main/java/edu/ucne/skyplanerent/presentation/login/FirstScreen.kt
+++ app/src/main/java/edu/ucne/skyplanerent/presentation/login/FirstScreen.kt
@@ -1,54 +1,80 @@
 package edu.ucne.skyplanerent.presentation.login
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import edu.ucne.skyplanerent.R
 import edu.ucne.skyplanerent.presentation.navigation.Screen
 
 @Composable
-fun FirstScreen(navController: NavHostController){
+fun FirstScreen(navController: NavHostController) {
 
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(24.dp),
-        verticalArrangement = Arrangement.Center
-    ) {
-        Text(text = "Reserva tu avion con solo unos clicks!", style = MaterialTheme.typography.headlineMedium)
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    )
+    {
+        Image(
+            painter = painterResource(id = R.drawable.c172hd),
+            contentDescription = "Logo Cessna 172 HD",
+            modifier = Modifier
+                .height(200.dp)
+                .fillMaxSize()
+        )
+        Spacer(modifier = Modifier.height(100.dp))
 
-        Spacer(modifier = Modifier.height(16.dp))
+            Text(
+                text = "Reserva tu avion con solo unos clicks!",
+                style = MaterialTheme.typography.headlineMedium
+            )
 
-        Button(
-            onClick = {
-              navController.navigate(Screen.Login(0))
-            },
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            Text("Iniciar sesión")
-        }
+            Spacer(modifier = Modifier.height(16.dp))
 
-        Spacer(modifier = Modifier.height(8.dp))
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
 
-        Button(
-            onClick = {
-              navController.navigate(Screen.Register)
-            },
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            Text("Registrarse")
-        }
+                Button(
+                    onClick = {
+                        navController.navigate(Screen.Login(0))
+                    },
+                    modifier = Modifier.width(135.dp)
+                ) {
+                    Text("Iniciar sesión")
+                }
 
+                Spacer(modifier = Modifier.height(8.dp))
+
+                Button(
+                    onClick = {
+                        navController.navigate(Screen.Register)
+                    },
+                    modifier = Modifier.width(135.dp)
+                ) {
+                    Text("Registrarse")
+                }
+            }
         }
     }
 
