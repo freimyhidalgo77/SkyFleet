687c7b10dd03c8944a2461a958052d290fb5bddb
fahh7 <fahh7@FREIMYHP>
2025-06-29 15:58:59 -0400

    ReservaRepository

---
+++ app/src/main/java/edu/ucne/skyplanerent/data/local/repository/ReservaRepository.kt
@@ -0,0 +1,20 @@
+package edu.ucne.skyplanerent.data.local.repository
+
+import edu.ucne.skyplanerent.data.local.dao.ReservaDao
+import edu.ucne.skyplanerent.data.local.entity.ReservaEntity
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class ReservaRepository @Inject constructor(
+    val reservaDao: ReservaDao
+
+){
+    suspend fun saveReserva(reserva: ReservaEntity) = reservaDao.save(reserva)
+
+    suspend fun findReserva(Id:Int):ReservaEntity = reservaDao.find(Id)
+
+    suspend fun deleteReserva(reserva: ReservaEntity) = reservaDao.delete(reserva)
+
+    fun getAll(): Flow<List<ReservaEntity>> = reservaDao.getAll()
+
+}
