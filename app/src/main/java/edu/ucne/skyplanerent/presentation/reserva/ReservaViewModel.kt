de0afa1e03f05350565a830021bc30f699768363
fahh7 <fahh7@FREIMYHP>
2025-06-29 15:58:28 -0400

    ReservaViewModel

---
+++ app/src/main/java/edu/ucne/skyplanerent/presentation/reserva/ReservaViewModel.kt
@@ -0,0 +1,171 @@
+package edu.ucne.skyplanerent.presentation.reserva
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import edu.ucne.skyplanerent.data.local.entity.ReservaEntity
+import edu.ucne.skyplanerent.data.local.repository.ReservaRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ReservaViewModel @Inject constructor(
+
+    private val reservaRepository: ReservaRepository
+
+): ViewModel() {
+
+    private val _uiState = MutableStateFlow(UiState())
+    val uiState get() = _uiState.asStateFlow()
+
+
+    init{
+        getReserva()
+
+    }
+
+    fun getReserva(){
+        viewModelScope.launch {
+            reservaRepository.reservaDao.getAll().collect{reserva->
+                _uiState.update {
+                    it.copy(reservas = reserva)
+                }
+            }
+        }
+    }
+
+    fun saveReserva(){
+        viewModelScope.launch {
+            try{
+                reservaRepository.saveReserva(_uiState.value.toEntity())
+                _uiState.update {
+                    it.copy(
+                        successMessage = "Reserva guardada con exito!", errorMessage = null
+                    )
+                }
+               // nuevaReserva()
+            }catch(e:Exception){
+                _uiState.update {
+                    it.copy(
+                        errorMessage = "Ha ocurrido un error al guardar la reserva", successMessage = null
+                    )
+                }
+            }
+        }
+    }
+
+    fun deleteReserva(){
+        viewModelScope.launch {
+            try{
+                reservaRepository.deleteReserva(_uiState.value.toEntity())
+                _uiState.update {
+                    it.copy(
+                        successMessage = "Reserva  eliminada!", errorMessage = null
+                    )
+                }
+
+            }catch(e:Exception){
+                _uiState.update {
+                    it.copy(
+                        errorMessage = "Ha ocurrido un error al eliminar la reserva"
+                    )
+                }
+            }
+        }
+
+    }
+
+    /*fun nuevaReserva(){
+        _uiState.update {
+            it.copy(
+                reservaId = null,
+
+            )
+        }
+
+    }*/
+
+    fun selectReserva(reservaId:Int){
+        viewModelScope.launch {
+            val reserva = reservaRepository.findReserva(reservaId)
+            if(reservaId > 0){
+                _uiState.update {
+                    it.copy(
+                        reservaId = reserva?.reservaId,
+                        rutaId = reserva?.rutaId,
+                        estadoId = reserva?.estadoId,
+                        formularioId = reserva?.formularioId,
+                        metodoPagoId = reserva?.metodoPagoId,
+                        tipoVueloId = reserva?.tipoVueloId,
+                        categoriaId = reserva?.categoriaId,
+                        pasajeros = reserva?.pasajeros,
+                        fecha = reserva?.fecha,
+                        impuesto = reserva?.impuesto?:0.0,
+                        tarifa = reserva.tarifa?:0.0
+
+                    )
+                }
+            }
+        }
+    }
+
+    fun nuevaReserva(){
+        _uiState.update {
+            it.copy(
+                successMessage = null, errorMessage = null
+            )
+        }
+    }
+
+    /*fun onChangeAreonave(aeronave:String){
+        _uiState.update {
+            it.copy(
+                aeronave = aeronave
+            )
+        }
+    }*/
+
+    /*fun onChangeFecha(Fecha:String){
+        _uiState.update {
+            it.copy(
+                descripcion = descripcion
+            )
+        }
+    }*/
+
+    fun onChangePasajeros(pasajero:Int){
+        _uiState.update {
+            it.copy(
+                pasajeros = pasajero
+            )
+        }
+
+    }
+
+    /*fun onChangeRuta(ruta:Int){
+        _uiState.update {
+            it.copy(
+                rutaId = ruta
+            )
+        }
+
+    }*/
+
+    fun UiState.toEntity() = ReservaEntity(
+        reservaId = reservaId,
+        rutaId = rutaId,
+        estadoId = estadoId,
+        formularioId = formularioId,
+        metodoPagoId = metodoPagoId,
+        tipoVueloId = tipoVueloId,
+        categoriaId = categoriaId,
+        fecha = fecha,
+        impuesto = impuesto?:0.0,
+        tarifa = tarifa?:0.0
+
+    )
+
+}
