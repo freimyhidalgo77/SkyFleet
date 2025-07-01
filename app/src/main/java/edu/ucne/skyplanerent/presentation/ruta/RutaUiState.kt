71b847c17c3c1786f02627a6950e6c94833b9f9b
frailyng <frailindes@gmail.com>
2025-06-30 20:50:29 -0400

    RutaUiState Creado

--- app/src/main/java/edu/ucne/skyplanerent/presentation/ruta/RutaUiState.kt
+++ app/src/main/java/edu/ucne/skyplanerent/presentation/ruta/RutaUiState.kt
@@ -1,2 +1,14 @@
 package edu.ucne.skyplanerent.presentation.ruta
 
+import edu.ucne.skyplanerent.data.local.entity.RutaEntity
+
+data class RutaUiState(
+    val rutaId: Int? = null,
+    val aeronaveId: Int? = null,
+    val origen: String = "",
+    val destino: String = "",
+    val distancia: String = "",
+    val duracionEstimada: String = "",
+    val errorMessage: String? = null,
+    val rutas: List<RutaEntity> = emptyList()
+)
