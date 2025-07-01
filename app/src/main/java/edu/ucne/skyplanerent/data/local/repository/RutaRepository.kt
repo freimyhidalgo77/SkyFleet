aba318919d2ef0b79163e8edd6330aaa3f13b6a2
frailyng <frailindes@gmail.com>
2025-06-30 20:36:40 -0400

    RutaRepository Creado

---
+++ app/src/main/java/edu/ucne/skyplanerent/data/local/repository/RutaRepository.kt
@@ -0,0 +1,21 @@
+package edu.ucne.skyplanerent.data.local.repository
+
+
+import edu.ucne.skyplanerent.data.local.dao.RutaDao
+import edu.ucne.skyplanerent.data.local.entity.RutaEntity
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class RutaRepository @Inject constructor(
+    val rutaDao: RutaDao
+
+){
+    suspend fun saveRuta(ruta: RutaEntity) = rutaDao.save(ruta)
+
+    suspend fun findRuta(Id:Int):RutaEntity = rutaDao.find(Id)
+
+    suspend fun deleteRuta(ruta: RutaEntity) = rutaDao.delete(ruta)
+
+    fun getAll(): Flow<List<RutaEntity>> = rutaDao.getAll()
+
+}
