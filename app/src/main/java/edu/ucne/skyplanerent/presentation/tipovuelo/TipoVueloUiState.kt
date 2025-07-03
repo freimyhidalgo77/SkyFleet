410ef3c007b9c526c90f3f483ad0864faf536e6c
frailyng <frailindes@gmail.com>
2025-07-03 10:50:40 -0400

    TipoVueloUiState Creado

--- app/src/main/java/edu/ucne/skyplanerent/presentation/tipovuelo/TipoVueloUiState.kt
+++ app/src/main/java/edu/ucne/skyplanerent/presentation/tipovuelo/TipoVueloUiState.kt
@@ -1,4 +1,13 @@
 package edu.ucne.skyplanerent.presentation.tipovuelo
 
-class TipoVueloUiState {
-}
+import edu.ucne.skyplanerent.data.local.entity.TipoVueloEntity
+
+data class TipoVueloUiState(
+    val vueloId: Int? = null,
+    val rutaId: Int = 0,
+    val tipoclienteId: Int = 0,
+    val descripcionTipoVuelo: String = "",
+    val precio: Double = 0.0,
+    val errorMessage: String? = null,
+    val tipovuelo: List<TipoVueloEntity> = emptyList()
+)
