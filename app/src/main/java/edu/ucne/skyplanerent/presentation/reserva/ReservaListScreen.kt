0b6fde32deb9402d175a9a4fa922b64f22ddf3cf
fahh7 <fahh7@FREIMYHP>
2025-07-04 01:47:52 -0400

    ReservaListScreen

--- app/src/main/java/edu/ucne/skyplanerent/presentation/reserva/ReservaListScreen.kt
+++ app/src/main/java/edu/ucne/skyplanerent/presentation/reserva/ReservaListScreen.kt
@@ -1,11 +1,13 @@
 package edu.ucne.skyplanerent.presentation.reserva
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -35,11 +37,13 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import edu.ucne.skyplanerent.R
 import edu.ucne.skyplanerent.data.local.entity.ReservaEntity
 import kotlinx.coroutines.CoroutineScope
 
@@ -80,12 +84,12 @@ fun ReservaBodyListScreen(
                     Text(
                         text = "Mis Reservas",
                         fontWeight = FontWeight.Bold,
-                        fontSize = 24.sp,
-                        color = Color.White
+                        fontSize = 26.sp,
+                        color = Color.Black
                     )
                 },
                 colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.primary
+                    containerColor = Color.White
                 )
             )
         },
@@ -100,6 +104,15 @@ fun ReservaBodyListScreen(
                 .fillMaxSize()
 
         ){
+
+            Image(
+                painter = painterResource(id = R.drawable.reservas),
+                contentDescription = "Imagen mis reservas",
+                modifier = Modifier
+                    .height(190.dp)
+                    .fillMaxSize()
+            )
+
             LazyColumn(
                 modifier = Modifier.fillMaxSize(),
                 verticalArrangement = Arrangement.spacedBy(10.dp)
