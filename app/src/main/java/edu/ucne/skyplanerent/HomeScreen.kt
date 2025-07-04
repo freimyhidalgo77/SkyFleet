fe1d98147f7c74115c9507bbe2e83cf8816b73cc
fahh7 <fahh7@FREIMYHP>
2025-07-04 01:12:04 -0400

    Home

--- app/src/main/java/edu/ucne/skyplanerent/HomeScreen.kt
+++ app/src/main/java/edu/ucne/skyplanerent/HomeScreen.kt
@@ -12,9 +12,23 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material.icons.filled.Person
+import androidx.navigation.NavController
+import androidx.navigation.compose.currentBackStackEntryAsState
 
 @Composable
-fun HomeScreen(onLogout: () -> Unit) {
+fun HomeScreen(
+    onLogout: () -> Unit,
+    onNavigateToReserva: () -> Unit
+) {
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -27,4 +41,15 @@ fun HomeScreen(onLogout: () -> Unit) {
             Text("Cerrar sesion")
         }
     }
+
+    Button(onClick = onNavigateToReserva) {
+        Text("Ver Reservas")
+    }
+
+
+
 }
+
+
+
+
