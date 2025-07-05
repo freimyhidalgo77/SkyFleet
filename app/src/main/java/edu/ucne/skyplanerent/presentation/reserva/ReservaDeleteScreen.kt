e65ba4f3b8d2201f81af3c58e33dd2920e191157
fahh7 <fahh7@FREIMYHP>
2025-07-05 10:18:52 -0400

    ReservaDelete

--- app/src/main/java/edu/ucne/skyplanerent/presentation/reserva/ReservaDeleteScreen.kt
+++ app/src/main/java/edu/ucne/skyplanerent/presentation/reserva/ReservaDeleteScreen.kt
@@ -1,4 +1,164 @@
 package edu.ucne.skyplanerent.presentation.reserva
 
-class ReservaDeleteScreen {
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ReservaDeleteScreen(
+    reservaId: Int,
+    viewModel: ReservaViewModel = hiltViewModel(),
+    goBack: () -> Unit
+) {
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    var showDialog by remember { mutableStateOf(false) }
+
+    LaunchedEffect(reservaId) {
+        viewModel.selectReserva(reservaId)
+    }
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(
+                        text = "Eliminar reserva",
+                        style = TextStyle(
+                            fontSize = 18.sp,
+                            fontWeight = FontWeight.Bold,
+                            color = Color.White
+                        )
+                    )
+                },
+                navigationIcon = {
+                    IconButton(onClick = goBack) {
+                        Icon(Icons.Filled.ArrowBack, contentDescription = "Regresar")
+                    }
+                },
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    containerColor = Color.White
+                )
+            )
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+                .padding(16.dp)
+        ) {
+            Text(
+                text = "¿Estás seguro de que quiere cancelar esta reserva?",
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold,
+
+            )
+            Spacer(modifier = Modifier.height(12.dp))
+
+            Text(
+                text = "Cancelar su reserva podría resultar en cargos por cancelación o afectar " +
+                        "su elegibilidad para un reembolso. Por favor, " +
+                        "revise los términos y condiciones antes de continuar.",
+                fontSize = 18.sp,
+
+                )
+            Spacer(modifier = Modifier.height(12.dp))
+
+            Text(text = "Fecha: ${uiState.fecha}", fontSize = 16.sp)
+            Text(text = "Ruta: ${uiState.rutaId}", fontSize = 16.sp)
+            Text(text = "Aeronave: ${uiState.categoriaId}", fontSize = 16.sp)
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                Button(
+                    onClick = goBack,
+                    colors = ButtonDefaults.buttonColors(containerColor = Color.Gray)
+                ) {
+                    Text(text = "Cancelar")
+                }
+
+                Button(
+                    onClick = { showDialog = true },
+                    colors = ButtonDefaults.buttonColors(containerColor = Color.Red)
+                ) {
+                    Icon(Icons.Filled.Delete, contentDescription = "Eliminar")
+                    Spacer(modifier = Modifier.width(4.dp))
+                    Text(text = "Eliminar")
+                }
+            }
+
+            if (uiState.errorMessage != null) {
+                Text(
+                    text = uiState.errorMessage!!,
+                    color = Color.Red,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(top = 16.dp)
+                )
+            }
+        }
+    }
+
+    if (showDialog) {
+        AlertDialog(
+            onDismissRequest = { showDialog = false },
+            title = { Text("Confirmar Eliminación") },
+            text = { Text("¿Realmente deseas eliminar este depósito? Esta acción no se puede deshacer.") },
+            confirmButton = {
+                Button(
+                    onClick = {
+                        viewModel.deleteReserva()
+                        showDialog = false
+                        goBack()
+                    },
+                    colors = ButtonDefaults.buttonColors(containerColor = Color.Red)
+                ) {
+                    Text("Eliminar")
+                }
+            },
+            dismissButton = {
+                Button(onClick = { showDialog = false }) {
+                    Text("Cancelar")
+                }
+            }
+        )
+    }
 }
