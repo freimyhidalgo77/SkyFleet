eb5eea09409f803aa9c693e35eecc4db659de984
frailyng <frailindes@gmail.com>
2025-07-14 23:46:17 -0400

    Ruta Repository Modificado

--- app/src/main/java/edu/ucne/skyplanerent/data/repository/RutaRepository.kt
+++ app/src/main/java/edu/ucne/skyplanerent/data/repository/RutaRepository.kt
@@ -1,13 +1,9 @@
 package edu.ucne.skyplanerent.data.repository
 
 import android.util.Log
-import edu.ucne.skyplanerent.data.local.dao.RutaDao
-import edu.ucne.skyplanerent.data.local.entity.RutaEntity
 import edu.ucne.skyplanerent.data.remote.Resource
 import edu.ucne.skyplanerent.data.remote.dto.RutaDTO
-import edu.ucne.skyplanerent.data.remote.dto.TipoVueloDTO
 import edu.ucne.skyplanerent.data.remote.rutas.RutasDataSource
-import edu.ucne.skyplanerent.data.remote.tiposVuelos.TipoVueloDataSource
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import retrofit2.HttpException
@@ -16,6 +12,21 @@ import javax.inject.Inject
 class RutaRepository @Inject constructor(
     private val dataSource: RutasDataSource
 ){
+    fun getRutas(rutaid: Int): Flow<Resource<List<RutaDTO>>> = flow {
+        try{
+            emit(Resource.Loading())
+            val ruta = dataSource.getRutas()
+            emit(Resource.Success(ruta))
+        }catch (e: HttpException){
+            val errorMessage = e.response()?.errorBody()?.string() ?: e.message()
+            Log.e("Ruta", "HttpException: $errorMessage")
+            emit(Resource.Error("Error de conexion $errorMessage"))
+        }catch (e: Exception){
+            Log.e("Ruta", "Exception: ${e.message}")
+            emit(Resource.Error("Error: ${e.message}"))
+
+        }
+    }
     fun getRuta(): Flow<Resource<List<RutaDTO>>> = flow {
         try{
             emit(Resource.Loading())
@@ -36,7 +47,7 @@ class RutaRepository @Inject constructor(
 
     suspend fun find(id: Int) = dataSource.getRuta(id)
 
-    suspend fun save(rutaDTO: RutaDTO) = dataSource.postRuta(rutaDTO)
+    suspend fun saveRuta(rutaDTO: RutaDTO) = dataSource.postRuta(rutaDTO)
 
     suspend fun delete(id: Int) = dataSource.deleteRuta(id)
 }
