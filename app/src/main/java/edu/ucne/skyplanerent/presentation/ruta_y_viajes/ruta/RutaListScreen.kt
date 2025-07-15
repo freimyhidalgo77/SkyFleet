3909385da7c3057590618ccef6f53b074b7ce7e9
frailyng <frailindes@gmail.com>
2025-07-14 23:45:56 -0400

    Ruta List Screen Creado

---
+++ app/src/main/java/edu/ucne/skyplanerent/presentation/ruta_y_viajes/ruta/RutaListScreen.kt
@@ -0,0 +1,199 @@
+package edu.ucne.skyplanerent.presentation.ruta_y_viajes.ruta
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material.pullrefresh.PullRefreshIndicator
+import androidx.compose.material.pullrefresh.pullRefresh
+import androidx.compose.material.pullrefresh.rememberPullRefreshState
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import edu.ucne.skyplanerent.data.remote.dto.RutaDTO
+
+@Composable
+fun RutaListScreen(
+    viewModel: RutaViewModel = hiltViewModel(),
+    createRuta: () -> Unit,
+    goToRuta: (Int) -> Unit,
+    goBack: () -> Unit
+) {
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
+    RutaListBodyScreen(
+        uiState = uiState,
+        goToRuta = { id -> goToRuta(id) },
+        onEvent = viewModel::onEvent,
+        createRuta = createRuta,
+        goBack = goBack
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterialApi::class)
+@Composable
+fun RutaListBodyScreen(
+    uiState: RutaUiState,
+    goToRuta: (Int) -> Unit,
+    onEvent: (RutaEvent) -> Unit,
+    createRuta: () -> Unit,
+    goBack: () -> Unit
+) {
+    val refreshing = uiState.isLoading
+    val pullRefreshState = rememberPullRefreshState(
+        refreshing = refreshing,
+        onRefresh = { onEvent(RutaEvent.GetRutas) }
+    )
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = " Rutas ",
+                        style = MaterialTheme.typography.titleLarge,
+                        color = Color.White
+                    )
+                },
+                navigationIcon = {
+                    IconButton(onClick = goBack) {
+                        Icon(
+                            Icons.Default.ArrowBack,
+                            contentDescription = "Volver",
+                            tint = Color.White
+                        )
+                    }
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Color(0xFF272D4D)
+                )
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = createRuta) {
+                Icon(Icons.Filled.Add, "Agregar nueva")
+            }
+        }
+    ) { padding ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .pullRefresh(pullRefreshState)
+                .padding(padding)
+        ) {
+            Column(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+
+                if (uiState.rutas.isEmpty()) {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(24.dp),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(
+                            text = "No hay rutas disponibles en este momento",
+                            style = MaterialTheme.typography.bodyLarge,
+                            color = Color.Gray
+                        )
+                    }
+                } else {
+                    LazyColumn(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp)
+                    ) {
+                        items(uiState.rutas) { ruta ->
+                            RutasRow(
+                                it = ruta,
+                                goToRuta = { goToRuta(ruta.rutaId ?: 0) }
+                            )
+                            Spacer(modifier = Modifier.height(8.dp))
+                        }
+                    }
+                }
+
+                if (!uiState.errorMessage.isNullOrEmpty()) {
+                    Box(
+                        modifier = Modifier
+                            .align(Alignment.CenterHorizontally)
+                            .padding(16.dp)
+                    ) {
+                        Text(
+                            text = uiState.errorMessage,
+                            style = MaterialTheme.typography.bodyMedium
+                        )
+                    }
+                }
+            }
+
+            PullRefreshIndicator(
+                refreshing = refreshing,
+                state = pullRefreshState,
+                modifier = Modifier.align(Alignment.TopCenter)
+            )
+        }
+    }
+}
+
+@Composable
+private fun RutasRow(
+    it: RutaDTO,
+    goToRuta: () -> Unit
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp)
+    ) {
+        Text(
+            modifier = Modifier.weight(1f),
+            text = "Ruta ${it.rutaId}",
+            color = Color.Black
+        )
+        Text(
+            modifier = Modifier.weight(2f),
+            text = "Origen: ${it.origen}, Destino: ${it.destino}",
+            style = MaterialTheme.typography.titleMedium,
+            color = Color.Blue
+        )
+        Text(
+            modifier = Modifier.weight(2f),
+            text = "Distancia: ${it.distancia}, Duracion: ${it.duracion}",
+            color = Color.Blue
+        )
+        IconButton(onClick = goToRuta) {
+            Icon(Icons.Default.ArrowForward, contentDescription = "Modificar/Eliminar", tint = MaterialTheme.colorScheme.primary)
+        }
+    }
+    HorizontalDivider()
+}
