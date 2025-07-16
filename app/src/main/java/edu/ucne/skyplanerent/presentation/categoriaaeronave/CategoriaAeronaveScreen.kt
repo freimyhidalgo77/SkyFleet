190343e353ce6886f689c05734c0caad5443e737
frailyng <frailindes@gmail.com>
2025-07-16 11:21:49 -0400

    Screen de categoria Creado

---
+++ app/src/main/java/edu/ucne/skyplanerent/presentation/categoriaaeronave/CategoriaAeronaveScreen.kt
@@ -0,0 +1,180 @@
+package edu.ucne.skyplanerent.presentation.categoriaaeronave
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+
+@Composable
+fun CategoriaAeronaveScreen(
+    categoriaId: Int?,
+    viewModel: CategoriaAeronaveViewModel = hiltViewModel(),
+    goBack: () -> Unit
+) {
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
+    LaunchedEffect(categoriaId) {
+        println("id $categoriaId")
+        categoriaId?.let {
+            if (it > 0) {
+                viewModel.selectedCategoria(it)
+            }
+        }
+    }
+    CategoriaAeronaveBodyScreen(
+        uiState = uiState,
+        onEvent = viewModel::onEvent,
+        goBack = goBack
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CategoriaAeronaveBodyScreen(
+    uiState: CategoriaAeronaveUiState,
+    onEvent: (CategoriaAeronaveEvent) -> Unit,
+    goBack: () -> Unit
+) {
+    val descripcionCategoriaError = uiState.descripcionCategoria.isNullOrBlank()
+    val isFormValid = !descripcionCategoriaError
+
+    Scaffold { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .padding(8.dp)
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                IconButton(
+                    onClick = goBack,
+                    modifier = Modifier.align(Alignment.CenterVertically)
+                ) {
+                    Icon(Icons.Default.ArrowBack, contentDescription = "volver")
+                }
+            }
+            ElevatedCard(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp)
+                ) {
+                    Spacer(modifier = Modifier.height(32.dp))
+                    Text("Registro de categorías de aeronaves")
+
+                    OutlinedTextField(
+                        value = uiState.categoriaId?.toString() ?: "Nuevo",
+                        onValueChange = {},
+                        modifier = Modifier.fillMaxWidth(),
+                        readOnly = true,
+                        enabled = false
+                    )
+
+                    OutlinedTextField(
+                        value = uiState.descripcionCategoria,
+                        onValueChange = { onEvent(CategoriaAeronaveEvent.DescripcionCategoriaChange(it)) },
+                        label = { Text("Descripción de la categoría") },
+                        modifier = Modifier.fillMaxWidth(),
+                        isError = descripcionCategoriaError,
+                        colors = OutlinedTextFieldDefaults.colors(
+                            focusedBorderColor = Color.Blue,
+                            unfocusedBorderColor = Color.Gray,
+                            focusedLabelColor = Color.Blue,
+                            errorBorderColor = Color.Red
+                        )
+                    )
+                    if (descripcionCategoriaError) {
+                        Text(
+                            text = "La descripción de la categoría no puede estar vacía",
+                            color = Color.Red,
+                            style = MaterialTheme.typography.bodyMedium,
+                            modifier = Modifier.padding(start = 16.dp, top = 4.dp)
+                        )
+                    }
+
+                    Spacer(modifier = Modifier.padding(2.dp))
+                    uiState.errorMessage?.let {
+                        Text(text = it, color = Color.Red)
+                    }
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        OutlinedButton(
+                            onClick = { onEvent(CategoriaAeronaveEvent.New) },
+                            colors = ButtonDefaults.outlinedButtonColors(
+                                contentColor = Color.Blue
+                            ),
+                            border = BorderStroke(1.dp, Color.Blue),
+                            modifier = Modifier.padding(horizontal = 8.dp)
+                        ) {
+                            Icon(
+                                imageVector = Icons.Default.Add,
+                                contentDescription = "new button"
+                            )
+                            Text("Nuevo")
+                        }
+                        OutlinedButton(
+                            onClick = {
+                                if (isFormValid) {
+                                    onEvent(CategoriaAeronaveEvent.Save)
+                                    goBack()
+                                }
+                            },
+                            enabled = isFormValid,
+                            colors = ButtonDefaults.outlinedButtonColors(
+                                contentColor = if (isFormValid) Color.Blue else Color.Gray,
+                                disabledContentColor = Color.Gray
+                            ),
+                            border = BorderStroke(1.dp, if (isFormValid) Color.Blue else Color.Gray),
+                            modifier = Modifier.padding(horizontal = 8.dp)
+                        ) {
+                            Icon(
+                                imageVector = Icons.Default.Edit,
+                                contentDescription = "save button"
+                            )
+                            Text(text = "Guardar")
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
