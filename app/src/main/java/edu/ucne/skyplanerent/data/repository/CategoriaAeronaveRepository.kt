0ce24d58d5186ec8009ea8ba49ffe0a966ab626f
frailyng <frailindes@gmail.com>
2025-07-16 10:44:57 -0400

    CateogriaAeronaveRepository Creado

---
+++ app/src/main/java/edu/ucne/skyplanerent/data/repository/CategoriaAeronaveRepository.kt
@@ -0,0 +1,15 @@
+package edu.ucne.skyplanerent.data.repository
+
+import edu.ucne.skyplanerent.data.local.dao.CategoriaAeronaveDao
+import edu.ucne.skyplanerent.data.local.entity.CategoriaAeronaveEntity
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class CategoriaAeronaveRepository @Inject constructor(
+    private val dao: CategoriaAeronaveDao
+) {
+    suspend fun saveCategoriaAeronave(categoriaaeronave: CategoriaAeronaveEntity) = dao.saveCategoriaAeronave(categoriaaeronave)
+    suspend fun find(id: Int): CategoriaAeronaveEntity? = dao.find(id)
+    suspend fun deleteCategoriaAeronave(categoriaaeronave: CategoriaAeronaveEntity) = dao.deleteCategoriaAeronave(categoriaaeronave)
+    fun getAll(): Flow<List<CategoriaAeronaveEntity>> = dao.getAll()
+}
