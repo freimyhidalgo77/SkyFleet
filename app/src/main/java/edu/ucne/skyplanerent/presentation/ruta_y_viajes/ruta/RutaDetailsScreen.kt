2abc036f7fb7cd10bcf49cbd7f2657d4a7b5c691
fahh7 <fahh7@FREIMYHP>
2025-07-17 14:09:23 -0400

    RutaDetailsScreen

--- app/src/main/java/edu/ucne/skyplanerent/presentation/ruta_y_viajes/ruta/RutaDetailsScreen.kt
+++ app/src/main/java/edu/ucne/skyplanerent/presentation/ruta_y_viajes/ruta/RutaDetailsScreen.kt
@@ -39,7 +39,7 @@ import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun RutaDetailsScreen(
+fun RutaScreenDetails(
     rutaId: Int?,
     viewModel: RutaViewModel = hiltViewModel(),
     goBack: () -> Unit,
