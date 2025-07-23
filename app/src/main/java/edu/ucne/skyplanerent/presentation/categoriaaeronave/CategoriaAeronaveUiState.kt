be89a88ab7b1d9445643f8513070ea239e4df8b1
frailyng <frailindes@gmail.com>
2025-07-22 23:52:29 -0400

    Categoria UiState con nuevo modelado de datos para soportar imagenes

--- app/src/main/java/edu/ucne/skyplanerent/presentation/categoriaaeronave/CategoriaAeronaveUiState.kt
+++ app/src/main/java/edu/ucne/skyplanerent/presentation/categoriaaeronave/CategoriaAeronaveUiState.kt
@@ -1,10 +1,12 @@
 package edu.ucne.skyplanerent.presentation.categoriaaeronave
 
+import android.net.Uri
 import edu.ucne.skyplanerent.data.local.entity.CategoriaAeronaveEntity
 
 data class CategoriaAeronaveUiState(
     val categoriaId: Int? = null,
     val descripcionCategoria: String = "",
     val errorMessage: String? = null,
-    val categorias: List<CategoriaAeronaveEntity> = emptyList() // Renombrado y ajustado al tipo CategoriaAeronave
+    val categorias: List<CategoriaAeronaveEntity> = emptyList(),
+    val imageUri: Uri? = null // Nuevo campo para la imagen
 )
