dfb0f679884ce5bf8639d1db75cb6f424b800f56
frailyng <frailindes@gmail.com>
2025-07-22 23:52:52 -0400

    Event modificado

--- app/src/main/java/edu/ucne/skyplanerent/presentation/categoriaaeronave/CategoriaAeronaveEvent.kt
+++ app/src/main/java/edu/ucne/skyplanerent/presentation/categoriaaeronave/CategoriaAeronaveEvent.kt
@@ -1,9 +1,12 @@
 package edu.ucne.skyplanerent.presentation.categoriaaeronave
 
+import android.net.Uri
+
 sealed interface CategoriaAeronaveEvent {
     data class CategoriaIdChange(val categoriaId: Int): CategoriaAeronaveEvent
     data class DescripcionCategoriaChange(val descripcionCategoria: String): CategoriaAeronaveEvent
     data object Save: CategoriaAeronaveEvent
     data object Delete: CategoriaAeronaveEvent
     data object New: CategoriaAeronaveEvent
+    data class ImageSelected(val uri: Uri) : CategoriaAeronaveEvent // Nuevo evento
 }
