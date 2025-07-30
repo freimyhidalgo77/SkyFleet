12f5682462cf37b67d0249055ed1030219ea9e07
frailyng <frailindes@gmail.com>
2025-07-30 13:18:11 -0400

    Db actualizada de version para soportar los Admins

--- app/src/main/java/edu/ucne/skyplanerent/data/local/database/SkyPlaneRentDB.kt
+++ app/src/main/java/edu/ucne/skyplanerent/data/local/database/SkyPlaneRentDB.kt
@@ -3,12 +3,14 @@ package edu.ucne.skyplanerent.data.local.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import edu.ucne.skyplanerent.data.local.dao.AdminDao
 import edu.ucne.skyplanerent.data.local.dao.AeronaveDao
 import edu.ucne.skyplanerent.data.local.dao.CategoriaAeronaveDao
 import edu.ucne.skyplanerent.data.local.dao.FormularioDao
 import edu.ucne.skyplanerent.data.local.dao.ReservaDao
 import edu.ucne.skyplanerent.data.local.dao.RutaDao
 import edu.ucne.skyplanerent.data.local.dao.TipoVueloDao
+import edu.ucne.skyplanerent.data.local.entity.AdminEntity
 import edu.ucne.skyplanerent.data.local.entity.AeronaveEntity
 import edu.ucne.skyplanerent.data.local.entity.CategoriaAeronaveEntity
 import edu.ucne.skyplanerent.data.local.entity.FormularioEntity
@@ -24,8 +26,9 @@ import edu.ucne.skyplanerent.data.local.entity.TipoVueloEntity
         FormularioEntity::class,
         AeronaveEntity::class,
         CategoriaAeronaveEntity::class,
+        AdminEntity::class
     ],
-    version = 17,
+    version = 18,
     exportSchema = false
 )
 
@@ -37,5 +40,6 @@ abstract class SkyPlaneRentDB : RoomDatabase() {
     abstract fun formularioDao():FormularioDao
     abstract fun aeronaveDao():AeronaveDao
     abstract fun categoriaAeronaveDao(): CategoriaAeronaveDao
+    abstract fun adminDao(): AdminDao
 
 }
