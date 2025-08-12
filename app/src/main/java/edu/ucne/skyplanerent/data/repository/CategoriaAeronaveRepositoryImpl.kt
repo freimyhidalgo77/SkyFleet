3ca4b5d5c2a40c46212a732debbae6e7f3194264
frailyng <frailindes@gmail.com>
2025-08-12 19:57:32 -0400

    CategoriaImpl Creado

---
+++ app/src/main/java/edu/ucne/skyplanerent/data/repository/CategoriaAeronaveRepositoryImpl.kt
@@ -0,0 +1,7 @@
+package edu.ucne.skyplanerent.data.repository
+
+import edu.ucne.skyplanerent.data.local.dao.CategoriaAeronaveDao
+import edu.ucne.skyplanerent.data.local.entity.CategoriaAeronaveEntity
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
